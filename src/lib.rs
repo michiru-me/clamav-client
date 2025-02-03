03df8718ef0308a81dd7e5fb1120c3354255c417
Luna Nova <luna@mint.lgbt>
2025-02-03 11:59:34 -0500

    feat: clean up files

--- src/lib.rs
+++ src/lib.rs
@@ -1,133 +1,176 @@
 use std::{
-    fs::File,
-    io::{Error, Read, Write},
-    net::{TcpStream, ToSocketAddrs},
-    path::Path,
-    result::Result,
-    str::{from_utf8, Utf8Error},
+  fs::File,
+  io::{Error, Read, Write},
+  net::{TcpStream, ToSocketAddrs},
+  path::Path,
+  result::Result,
+  str::{from_utf8, Utf8Error}
 };
 
 type IoResult = Result<Vec<u8>, Error>;
 type Utf8Result = Result<bool, Utf8Error>;
 
-const DEFAULT_CHUNK_SIZE: u32 = 4096; // 4 kibibytes
+// 4 kibibytes
+const DEFAULT_CHUNK_SIZE: u32 = 4096;
 
 fn ping<RW>(mut stream: RW) -> IoResult
 where
-    RW: Read + Write,
+  RW: Read + Write
 {
-    stream.write_all(b"zPING\0")?;
+  stream.write_all(b"zPING\0")?;
 
-    let capacity = b"PONG\0".len();
-    let mut response = Vec::with_capacity(capacity);
-    stream.read_to_end(&mut response)?;
-    Ok(response)
+  let capacity = b"PONG\0".len();
+  let mut response = Vec::with_capacity(capacity);
+
+  stream.read_to_end(&mut response)?;
+
+  Ok(response)
 }
 
-fn scan<P, RW>(file_path: P, chunk_size: Option<u32>, mut stream: RW) -> IoResult
+fn scan<P, RW>(
+  file_path: P,
+  chunk_size: Option<u32>,
+  mut stream: RW
+) -> IoResult
 where
-    P: AsRef<Path>,
-    RW: Read + Write,
+  P: AsRef<Path>,
+  RW: Read + Write
 {
-    stream.write_all(b"zINSTREAM\0")?;
+  stream.write_all(b"zINSTREAM\0")?;
 
-    let chunk_size = chunk_size.unwrap_or(DEFAULT_CHUNK_SIZE);
-    let mut file = File::open(file_path)?;
-    let mut buffer = vec![0; chunk_size as usize];
-    loop {
-        let len = file.read(&mut buffer[..])?;
-        if len != 0 {
-            stream.write_all(&(len as u32).to_be_bytes())?;
-            stream.write_all(&buffer[0..len])?;
-        } else {
-            stream.write_all(&[0; 4])?;
-            break;
-        }
+  let chunk_size = chunk_size.unwrap_or(DEFAULT_CHUNK_SIZE);
+
+  let mut buffer = vec![0; chunk_size as usize];
+  let mut file = File::open(file_path)?;
+
+  loop {
+    let len = file.read(&mut buffer[..])?;
+
+    if len != 0 {
+      stream.write_all(&(len as u32).to_be_bytes())?;
+      stream.write_all(&buffer[0..len])?;
+    } else {
+      stream.write_all(&[0; 4])?;
+
+      break;
     }
+  }
 
-    let mut response = Vec::new();
-    stream.read_to_end(&mut response)?;
-    Ok(response)
+  let mut response = Vec::new();
+
+  stream.read_to_end(&mut response)?;
+
+  Ok(response)
 }
 
-fn scan_buffer<RW>(buffer: &[u8], mut stream: RW, chunk_size: Option<u32>) -> IoResult
+fn scan_buffer<RW>(
+  buffer: &[u8],
+  mut stream: RW,
+  chunk_size: Option<u32>
+) -> IoResult
 where
-    RW: Read + Write,
+  RW: Read + Write
 {
-    stream.write_all(b"zINSTREAM\0")?;
+  stream.write_all(b"zINSTREAM\0")?;
 
-    let chunk_size = chunk_size.unwrap_or(DEFAULT_CHUNK_SIZE) as usize;
-    for chunk in buffer.chunks(chunk_size) {
-        stream.write_all(&(chunk.len() as u32).to_be_bytes())?;
-        stream.write_all(&chunk[..])?;
-    }
+  let chunk_size = chunk_size.unwrap_or(DEFAULT_CHUNK_SIZE) as usize;
 
-    stream.write_all(&[0; 4])?;
+  for chunk in buffer.chunks(chunk_size) {
+    stream.write_all(&(chunk.len() as u32).to_be_bytes())?;
+    stream.write_all(&chunk[..])?;
+  }
 
-    let mut response = Vec::new();
-    stream.read_to_end(&mut response)?;
-    Ok(response)
+  stream.write_all(&[0; 4])?;
+
+  let mut response = Vec::new();
+
+  stream.read_to_end(&mut response)?;
+
+  Ok(response)
+}
+
+pub fn clean(response: &[u8]) -> Utf8Result {
+  let response = from_utf8(response)?;
+
+  Ok(response.contains("OK") && !response.contains("FOUND"))
 }
 
 #[cfg(target_family = "unix")]
 pub fn ping_socket<P>(socket_path: P) -> IoResult
 where
-    P: AsRef<Path>,
+  P: AsRef<Path>
 {
-    use std::os::unix::net::UnixStream;
+  use std::os::unix::net::UnixStream;
 
-    let stream = UnixStream::connect(socket_path)?;
-    ping(stream)
-}
+  let stream = UnixStream::connect(socket_path)?;
 
-#[cfg(target_family = "unix")]
-pub fn scan_socket<P>(file_path: P, socket_path: P, chunk_size: Option<u32>) -> IoResult
-where
-    P: AsRef<Path>,
-{
-    use std::os::unix::net::UnixStream;
-
-    let stream = UnixStream::connect(socket_path)?;
-    scan(file_path, chunk_size, stream)
-}
-
-#[cfg(target_family = "unix")]
-pub fn scan_buffer_socket<P>(buffer: &[u8], socket_path: P, chunk_size: Option<u32>) -> IoResult
-where
-    P: AsRef<Path>,
-{
-    use std::os::unix::net::UnixStream;
-
-    let stream = UnixStream::connect(socket_path)?;
-    scan_buffer(buffer, stream, chunk_size)
+  ping(stream)
 }
 
 pub fn ping_tcp<A>(host_address: A) -> IoResult
 where
-    A: ToSocketAddrs,
+  A: ToSocketAddrs
 {
-    let stream = TcpStream::connect(host_address)?;
-    ping(stream)
+  let stream = TcpStream::connect(host_address)?;
+
+  ping(stream)
 }
 
-pub fn scan_tcp<P, A>(file_path: P, host_address: A, chunk_size: Option<u32>) -> IoResult
+#[cfg(target_family = "unix")]
+pub fn scan_buffer_socket<P>(
+  buffer: &[u8],
+  socket_path: P,
+  chunk_size: Option<u32>
+) -> IoResult
 where
-    A: ToSocketAddrs,
-    P: AsRef<Path>,
+  P: AsRef<Path>
 {
-    let stream = TcpStream::connect(host_address)?;
-    scan(file_path, chunk_size, stream)
+  use std::os::unix::net::UnixStream;
+
+  let stream = UnixStream::connect(socket_path)?;
+
+  scan_buffer(buffer, stream, chunk_size)
 }
 
-pub fn scan_buffer_tcp<A>(buffer: &[u8], host_address: A, chunk_size: Option<u32>) -> IoResult
+pub fn scan_buffer_tcp<A>(
+  buffer: &[u8],
+  host_address: A,
+  chunk_size: Option<u32>
+) -> IoResult
 where
-    A: ToSocketAddrs,
+  A: ToSocketAddrs
 {
-    let stream = TcpStream::connect(host_address)?;
-    scan_buffer(buffer, stream, chunk_size)
+  let stream = TcpStream::connect(host_address)?;
+
+  scan_buffer(buffer, stream, chunk_size)
 }
 
-pub fn clean(response: &[u8]) -> Utf8Result {
-    let response = from_utf8(response)?;
-    Ok(response.contains("OK") && !response.contains("FOUND"))
+#[cfg(target_family = "unix")]
+pub fn scan_socket<P>(
+  file_path: P,
+  socket_path: P,
+  chunk_size: Option<u32>
+) -> IoResult
+where
+  P: AsRef<Path>
+{
+  use std::os::unix::net::UnixStream;
+
+  let stream = UnixStream::connect(socket_path)?;
+
+  scan(file_path, chunk_size, stream)
+}
+
+pub fn scan_tcp<P, A>(
+  file_path: P,
+  host_address: A,
+  chunk_size: Option<u32>
+) -> IoResult
+where
+  A: ToSocketAddrs,
+  P: AsRef<Path>
+{
+  let stream = TcpStream::connect(host_address)?;
+
+  scan(file_path, chunk_size, stream)
 }
--- tests/clamav_client.rs
+++ tests/clamav_client.rs
@@ -1,74 +1,107 @@
-use revolt_clamav_client::*;
+use clamav_client::*;
+
+const EICAR_BUFFER_FOUND_RESPONSE: &[u8] = b"stream: Win.Test.EICAR_HDB-1 FOUND\0";
+
+const EICAR_FOUND_RESPONSE: &[u8] = b"stream: Eicar-Signature FOUND\0";
+
+const PONG_RESPONSE: &[u8] = b"PONG\0";
+
+const TEST_FILE_PATH: &str = "tests/eicar.txt";
+
+const TEST_HOST_ADDRESS: &str = "localhost:3310";
 
 #[cfg(target_family = "unix")]
 const TEST_SOCKET_PATH: &str = "/tmp/clamd.socket";
-const TEST_HOST_ADDRESS: &str = "localhost:3310";
-const TEST_FILE_PATH: &str = "tests/eicar.txt";
-
-const PONG_RESPONSE: &[u8] = b"PONG\0";
-const EICAR_FOUND_RESPONSE: &[u8] = b"stream: Eicar-Signature FOUND\0";
-const EICAR_BUFFER_FOUND_RESPONSE: &[u8] = b"stream: Win.Test.EICAR_HDB-1 FOUND\0";
 
 #[test]
 #[cfg(target_family = "unix")]
 fn test_ping_socket() {
-    let err_msg = format!(
-        "Could not ping clamd via Unix socket at {}",
-        TEST_SOCKET_PATH
-    );
-    let response = ping_socket(TEST_SOCKET_PATH).expect(&err_msg);
-    assert_eq!(&response, PONG_RESPONSE);
+  let err_msg = format!(
+    "Could not ping clamd via Unix socket at {}",
+    TEST_SOCKET_PATH
+  );
+
+  let response = ping_socket(TEST_SOCKET_PATH).expect(&err_msg);
+
+  assert_eq!(&response, PONG_RESPONSE);
 }
 
 #[test]
 fn test_ping_tcp() {
-    let err_msg = format!("Could not ping clamd via TCP at {}", TEST_HOST_ADDRESS);
-    let response = ping_tcp(TEST_HOST_ADDRESS).expect(&err_msg);
-    assert_eq!(&response, PONG_RESPONSE);
+  let err_msg = format!(
+    "Could not ping clamd via TCP at {}",
+    TEST_HOST_ADDRESS
+  );
+
+  let response = ping_tcp(TEST_HOST_ADDRESS).expect(&err_msg);
+
+  assert_eq!(&response, PONG_RESPONSE);
+}
+
+#[test]
+fn test_scan_buffer_tcp() {
+  let err_msg = format!(
+    "Could not scan EICAR string via TCP at {}",
+    TEST_HOST_ADDRESS
+  );
+
+  let buffer =
+    "X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*"
+      .bytes()
+      .collect::<Vec<u8>>();
+
+  let response = scan_buffer_tcp(
+    &buffer,
+    TEST_HOST_ADDRESS,
+    None
+  ).expect(&err_msg);
+
+  assert_eq!(&response, EICAR_BUFFER_FOUND_RESPONSE);
+
+  let is_clean = clean(&response);
+
+  assert_eq!(is_clean, Ok(false));
 }
 
 #[test]
 #[cfg(target_family = "unix")]
 fn test_scan_socket() {
-    let err_msg = format!(
-        "Could not scan test file {} via socket at {}",
-        TEST_FILE_PATH, TEST_SOCKET_PATH
-    );
+  let err_msg = format!(
+    "Could not scan test file {} via socket at {}",
+    TEST_FILE_PATH,
+    TEST_SOCKET_PATH
+  );
 
-    let response = scan_socket(TEST_FILE_PATH, TEST_SOCKET_PATH, None).expect(&err_msg);
-    assert_eq!(&response, EICAR_FOUND_RESPONSE);
+  let response = scan_socket(
+    TEST_FILE_PATH,
+    TEST_SOCKET_PATH,
+    None
+  ).expect(&err_msg);
 
-    let is_clean = clean(&response);
-    assert_eq!(is_clean, Ok(false));
+  assert_eq!(&response, EICAR_FOUND_RESPONSE);
+
+  let is_clean = clean(&response);
+
+  assert_eq!(is_clean, Ok(false));
 }
 
 #[test]
 fn test_scan_tcp() {
-    let err_msg = format!(
-        "Could not scan test file {} via TCP at {}",
-        TEST_FILE_PATH, TEST_HOST_ADDRESS
-    );
-    let response = scan_tcp(TEST_FILE_PATH, TEST_HOST_ADDRESS, None).expect(&err_msg);
-    assert_eq!(&response, EICAR_FOUND_RESPONSE);
+  let err_msg = format!(
+    "Could not scan test file {} via TCP at {}",
+    TEST_FILE_PATH,
+    TEST_HOST_ADDRESS
+  );
 
-    let is_clean = clean(&response);
-    assert_eq!(is_clean, Ok(false));
-}
+  let response = scan_tcp(
+    TEST_FILE_PATH,
+    TEST_HOST_ADDRESS,
+    None
+  ).expect(&err_msg);
 
-#[test]
-fn test_scan_buffer_tcp() {
-    let err_msg = format!(
-        "Could not scan EICAR string via TCP at {}",
-        TEST_HOST_ADDRESS
-    );
+  assert_eq!(&response, EICAR_FOUND_RESPONSE);
 
-    let buffer = "X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*"
-        .bytes()
-        .collect::<Vec<u8>>();
+  let is_clean = clean(&response);
 
-    let response = scan_buffer_tcp(&buffer, TEST_HOST_ADDRESS, None).expect(&err_msg);
-    assert_eq!(&response, EICAR_BUFFER_FOUND_RESPONSE);
-
-    let is_clean = clean(&response);
-    assert_eq!(is_clean, Ok(false));
+  assert_eq!(is_clean, Ok(false));
 }
